97e6abbc4adc0ce989067826f5bcd26828be931f
Sylvia-J <sylviajebett@gmail.com>
2020-05-26 14:54:28 +0300

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+function add(num1, num2) {
+  return num1 + num2;
+}
+var num1 = parseInt(prompt("Enter an integer: "));
+var num2 = parseInt(prompt("Enter another number: "));
+alert(add(num1, num2));
